beb48bd4f77bdf4a91057f3164a86727e4eec3e3
Ivan Shkardybarda <ivanshkardybarda@gmail.com>
2021-01-19 21:37:20 +0200

    update program, add new function which is much faster as initial one

---
+++ README.md
@@ -0,0 +1,4 @@
+Simple program to find a unique first letter of the input text:
+
+main.py -> main file with a program, it reads text from the stdin
+test.txt -> file with test data (sample from the form)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+from collections import OrderedDict
+import time
 
 
 def find_unique(array: list) -> list:
@@ -11,9 +13,36 @@ def find_unique(array: list) -> list:
     return result_array
 
 
+def find_unique2(array: list) -> list:
+    result_dict = OrderedDict()
+    for word in array:
+        hash_dict = OrderedDict()
+        for letter in word:
+            if letter not in hash_dict.keys():
+                hash_dict.update({letter: 1})
+            else:
+                hash_dict[letter] += 1
+        for key, val in hash_dict.items():
+            if val == 1:
+                if key not in result_dict.keys():
+                    result_dict.update({key: 1})
+                else:
+                    result_dict[key] += 1
+                break
+    for res_key, res_val in result_dict.items():
+        if res_val == 1:
+            return res_key
+
+
 if __name__ == '__main__':
     data = sys.stdin.read().replace(".", " ").replace("-", " ")
     splited = data.split()
+    start_time = time.time()
     part_res = find_unique(splited)
     result = find_unique(["".join(part_res)])
-    print("Resul of finding unique is -> '{}'".format(result[0]))
+    print("Result of finding unique is -> '{}'".format(result[0]))
+    print("Time to execute var1 == ", time.time() - start_time)
+    start_time = time.time()
+    result = find_unique2(splited)
+    print("Result of finding unique is -> '{}'".format(result[0]))
+    print("Time to execute var2 == ", time.time() - start_time)
